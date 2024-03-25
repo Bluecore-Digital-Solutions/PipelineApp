e36354f18b314b925267a2f8c9014eb16dba87a3
Chidubem Nwodo <chidubem.nwodo@atrizult.com>
2024-03-25 11:27:34 +0100

    subcription ui

--- src/pages/Subcription/Subcription.tsx
+++ src/pages/Subcription/Subcription.tsx
@@ -14,95 +14,77 @@ import {
   IonRow,
   IonTitle,
   IonToolbar,
-} from '@ionic/react';
-import { arrowBack, arrowForward } from 'ionicons/icons';
-import React from 'react';
-import './Subcription.css';
+} from "@ionic/react";
+import { arrowBack, arrowForward } from "ionicons/icons";
+import React from "react";
+import "./Subcription.css";
 // import '../theme/variables.css';
 
 const Subscription: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
-        <IonToolbar className='ion-padding'>
-          <IonButton className='button-1' slot='start'>
-            <IonBackButton className='back' defaultHref='/home'></IonBackButton>
+        <IonToolbar className="ion-padding">
+          <IonButton className="button-1" slot="start">
+            <IonBackButton className="back" defaultHref="/home"></IonBackButton>
             {/* <IonIcon className='back' defaultHref='/home' icon={arrowBack} /> */}
           </IonButton>
           <IonTitle>Subscription</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent className='ion-padding'>
-        <IonList className='custom-list'>
-          <div className='sub-menu'>
-            <div className='writeup'>Subcription level</div>
-            <div className='button-2' role='button'>
+      <IonContent className="ion-padding">
+        <IonList className="custom-list">
+          <div className="sub-menu">
+            <div className="writeup">Subcription level</div>
+            <div className="button-2" role="button">
               Upgrade/review <IonIcon icon={arrowForward} />
             </div>
           </div>
-
-          {/* <IonRadioGroup className='custom-rg'>
-            <IonItem className='custom-item'>
-              <IonRadio slot='start' value='bronze' className='custom-radio' />
-              <IonLabel className='custom-label'>Bronze</IonLabel>
-            </IonItem>
-            <IonItem className='custom-item'>
-              <IonRadio slot='start' value='silver' className='custom-radio' />
-              <IonLabel className='custom-label'>Silver</IonLabel>
-            </IonItem>
-            <IonItem className='custom-item'>
-              <IonRadio slot='start' value='gold' className='custom-radio' />
-              <IonLabel className='custom-label'>Gold</IonLabel>
-            </IonItem>
-            <IonItem className='custom-item'>
-              <IonRadio
-                slot='start'
-                value='platinum'
-                className='custom-radio'
+          <div className="click-option">
+            <div className="click-item">
+              <input type="radio" name="bronze" id="bronze" value="bronze" />
+              <label className="label-item" id="bronze">
+                Bronze
+              </label>
+            </div>
+            <div className="click-item">
+              <input type="radio" name="bronze" id="silver" value="silver" />
+              <label className="label-item">Silver</label>
+            </div>
+            <div className="click-item">
+              <input type="radio" name="bronze" id="gold" value="gold" />
+              <label className="label-item" id="gold">
+                Gold
+              </label>
+            </div>
+            <div className="click-item">
+              <input
+                type="radio"
+                name="bronze"
+                id="platinum"
+                value="platinum"
               />
-              <IonLabel className='custom-label'>Platinum</IonLabel>
-            </IonItem>
-            <IonItem className='custom-item'>
-              <IonRadio slot='start' value='sappire' className='custom-radio' />
-              <IonLabel className='custom-label'>Sapphire</IonLabel>
-            </IonItem>
-          </IonRadioGroup> */}
-          <div className='click-option'>
-            <div className='click-item'>
-              <input type='radio' id='bronze' value='bronze' />
-              <label className='label-item'>Bronze</label>
+              <label className="label-item">Platinum</label>
             </div>
-            <div className='click-item'>
-              <input type='radio' value='silver' />
-              <label className='label-item'>Silver</label>
-            </div>
-            <div className='click-item'>
-              <input type='radio' value='gold' checked />
-              <label className='label-item'>Gold</label>
-            </div>
-            <div className='click-item'>
-              <input type='radio' value='platinum' />
-              <label className='label-item'>Platinum</label>
-            </div>
-            <div className='click-item'>
-              <input type='radio' value='sappire' />
-              <label className='label-item'>Sapphire</label>
+            <div className="click-item">
+              <input type="radio" name="bronze" id="sappire" value="sappire" />
+              <label className="label-item">Sapphire</label>
             </div>
           </div>
         </IonList>
-        <div className='plan'>
-          <div className='plan-menu'>
-            <div className='plan-item'>Gold PLAN</div>
+        <div className="plan">
+          <div className="plan-menu">
+            <div className="plan-item">Gold PLAN</div>
             <div>
-              <div className='upgrade-btn'>Upgrade/review</div>
+              <div className="upgrade-btn">Upgrade/review</div>
             </div>
           </div>
-          <IonList className='plan ion-padding'>
+          <IonList className="plan ion-padding">
             <p>Dec 2023</p>
             <p>Silver plan upgrade</p>
-            <div className='approved'>
-              <p slot='start'>N10,000</p>
-              <p className='approved-p'>Approved</p>
+            <div className="approved">
+              <p slot="start">N10,000</p>
+              <p className="approved-p">Approved</p>
             </div>
           </IonList>
         </div>
